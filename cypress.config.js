5ec89ba29886050a41739c8f0ab5260bb2037b9b
WiktoriaKalisz <wikikalisz@gmail.com>
2026-01-11 19:09:13 +0100

    Add CI workflow

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://127.0.0.1:9292',
+    
     setupNodeEvents(on, config) {
     },
+
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
     videosFolder: 'cypress/videos',
     screenshotsFolder: 'cypress/screenshots',
   },
+  
   component: {
     devServer: {
       framework: 'next',
